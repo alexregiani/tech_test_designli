8a5976a03d107ddf522bddbb76f66839ba7a66bf
Alex Regiani <alexregianideveloper@outlook.com>
2024-06-26 20:25:28 -0400

    default to mock

--- lib/core/get_it/blocs/get_it_configuration_service.dart
+++ lib/core/get_it/blocs/get_it_configuration_service.dart
@@ -3,6 +3,6 @@ import 'package:tech_test_designli/core/server_configuration_service.dart';
 
 void getItConfigurationService() {
   sl.registerSingleton<ServerConfigurationService>(
-    ServerConfigurationService(useMockServer: false),
+    ServerConfigurationService(useMockServer: true),
   );
 }
